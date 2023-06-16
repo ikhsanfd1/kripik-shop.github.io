2343736e5ce5fab056b27badbbf1025d727bba3f
ikhsanfd1 <dilah80999@gmail.com>
2023-06-16 16:15:35 +0700

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="mt-5 mb-5">
+    <hr />
+    <div class="container">
+      <div class="row">
+        <div class="col text-center">2023 Copyright Ikhsan FD Academy</div>
+      </div>
+    </div>
+  </footer>
+</template>
+  
+  <script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Footer",
+};
+</script>
+  
+  <style>
+</style>
